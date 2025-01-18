f1f8fc2733edf3810b2c90d6fbdbbb2514c18544
Arnold Jabea <aroljabea71@gmail.com>
2025-01-18 21:13:01 +0100

    API server configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        
+      },
+       "/public": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        
+      },
+    },
+    
+  },
   plugins: [react()],
 });
