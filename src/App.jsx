0a2ed6bcf982938bea5d09648bd39331cb8932a4
Arnold Jabea <aroljabea71@gmail.com>
2025-01-19 00:00:14 +0100

    creating the component for the order

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 
 
@@ -7,21 +7,7 @@ const App = () => {
   return (
     <div>
       <h1>  Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="pep, cheese, and tomatoes"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="ham, pineapple, and mozzarella"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Americano"
-        description="french fries, hot dogs, and ketchup"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
     </div>
   );
     
